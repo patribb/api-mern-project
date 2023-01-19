2496ce9a4cc840382191ddd4236f9ebd1b6dd738
Patricia Garcia <martinez171214@gmail.com>
2023-01-19 09:42:28 +0100

    feat: added code style configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 const server = express();
 server.use(express.json());
 
+
 server.get('/', (_req: Request, res: Response) => {
     res.send('Hello World from server!');
 })
